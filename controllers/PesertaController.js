405f107b949707bea390081923a78c211c38f5a2
Dhanar J Kusuma <Dhanar J Kusuma>
2017-03-09 16:13:22 +0700

    test

--- controllers/PesertaController.js
+++ controllers/PesertaController.js
@@ -85,7 +85,6 @@ exports.updateCtrl = function(req, res, next){
                         _location : peserta._location
                     });
                     revisi.save(function(err){
-                        console.log(revisi);
                         peserta._revisi.push(revisi._id);
                         peserta.nama = req.body.name;
                         peserta.alamat = req.body.alamat;
